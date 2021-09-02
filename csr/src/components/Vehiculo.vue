d3200b285d63e631e2db3fcd4f0464bd3d6b9761
JoelMndz <luisjo3lml@gmail.com>
2021-09-02 13:04:37 -0500

    ok

---
+++ csr/src/components/Vehiculo.vue
@@ -0,0 +1,30 @@
+<template>
+   <h1> Vehiculos </h1> 
+    <FormV />
+</template>
+
+<script>
+import FormV from './FormV.vue'
+import {provide, reactive, ref} from 'vue'
+
+export default {
+  name: 'Vehiculo',
+  components: {
+    FormV
+  },
+  setup(){
+    const vehiculos = ref([]);
+    const vehiculo = reactive({
+      nombre:'',
+      placa:'',
+      espacio:'',
+      numeroTicket:'',
+      horas:0,
+      costo:0
+    })
+    provide('vehiculo',vehiculo);
+    provide('vehiculos', vehiculos);
+  }
+}
+</script>
+
